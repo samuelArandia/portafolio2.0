e96b3118663ca6316791813a968eee4c87235476
Samuel Arandia <samuelarandia@gmail.com>
2024-05-23 19:08:49 -0400

    Add new infinity scroll

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,19 +1,41 @@
 import React, { useEffect } from 'react';
-import { FaVuejs, FaPython, FaHtml5, FaReact, FaCss3Alt, FaAngular, FaJava } from 'react-icons/fa';
+import { FaVuejs, FaPython, FaHtml5, FaReact, FaCss3Alt, FaAngular, FaJava, FaGithub } from 'react-icons/fa';
 import { GiSkills } from 'react-icons/gi';
 import { DiDjango } from 'react-icons/di';
-import { SiPostgresql,SiNestjs,SiJetbrains , SiSpringboot ,SiTypescript, SiAzuredevops,SiPostman , SiJavascript, SiMicrosoftazure, SiDocker  } from 'react-icons/si';
+import { SiPostgresql, SiNestjs, SiJetbrains, SiSpringboot, SiTypescript, SiAzuredevops, SiPostman, SiJavascript, SiMicrosoftazure, SiDocker } from 'react-icons/si';
 import { BsGit } from 'react-icons/bs';
 import { TbBrandVscode } from 'react-icons/tb';
-import { GrGraphQl } from "react-icons/gr";
-import { FaGithub } from "react-icons/fa";
+import { GrGraphQl } from 'react-icons/gr';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import IconBox from './iconBox'; // El nuevo componente
+import IconBox from './iconBox';
 
 const Skills = () => {
   useEffect(() => {
     Aos.init();
+  }, []);
+
+  useEffect(() => {
+    const scrollers = document.querySelectorAll('.scroller');
+
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      addAnimation();
+    }
+
+    function addAnimation() {
+      scrollers.forEach((scroller) => {
+        scroller.setAttribute('data-animated', 'true');
+        const scrollerInner = scroller.querySelector('.scroller__inner');
+        if (scrollerInner) {
+          const scrollerContent = Array.from(scrollerInner.children);
+          scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', 'true');
+            scrollerInner.appendChild(duplicatedItem);
+          });
+        }
+      });
+    }
   }, []);
 
   return (
@@ -25,9 +47,8 @@ const Skills = () => {
       <p className='text-lg mb-4 text-center'>
         Estas son algunas de las tecnologías y lenguajes de programación en las que tengo experiencia y herramientas que uso diariamente.
       </p>
-
-      <div className=' overflow-hidden text-center'>
-        <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 text-center justify-items-center content-center justify-center p-10 md:p-20 gap-4' data-aos='fade-up'>
+      <div className='scroller' data-direction='left' data-speed='slow'>
+        <div className='scroller__inner'>
           {/* Lenguajes de Programación */}
           <IconBox Icon={FaHtml5} color='text-red-500' label='HTML' />
           <IconBox Icon={FaCss3Alt} color='text-blue-500' label='CSS' />
@@ -35,7 +56,10 @@ const Skills = () => {
           <IconBox Icon={SiTypescript} color='text-blue-500' label='TypeScript' />
           <IconBox Icon={FaPython} color='text-blue-500' label='Python' />
           <IconBox Icon={FaJava} color='text-red-500' label='Java' />
-
+          </div>
+      </div>
+      <div className='scroller' data-direction='right' data-speed='slow'>
+        <div className='scroller__inner'>
           {/* Frameworks */}
           <IconBox Icon={FaVuejs} color='text-green-700' label='Vue.js' />
           <IconBox Icon={FaReact} color='text-sky-700' label='React' />
@@ -44,7 +68,10 @@ const Skills = () => {
           <IconBox Icon={GrGraphQl} color='text-pink-500' label='GraphQL' />
           <IconBox Icon={SiNestjs} color='text-red-500' label='NestJS' />
           <IconBox Icon={SiSpringboot} color='text-green-500' label='Spring Boot' />
-
+          </div>
+      </div>
+      <div className='scroller' data-direction='left' data-speed='slow'>
+        <div className='scroller__inner'>
           {/* Herramientas */}
           <IconBox Icon={SiPostgresql} color='text-blue-500' label='PostgreSQL' />
           <IconBox Icon={SiAzuredevops} color='text-blue-500' label='Azure DevOps' />
